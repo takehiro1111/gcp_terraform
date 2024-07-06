a30a9ccc1676f0b436743757d5c8c8d8a54a2f8d
takehiro1111 <t.soccer.0916@gmail.com>
2024-07-06 16:02:04 +0900

    不要なファイルの削除

--- first/.tflint.hcl
+++ first/.tflint.hcl
@@ -9,6 +9,10 @@ rule "terraform_standard_module_structure"  {
   enabled = false
 }
 
+rule "terraform_deprecated_interpolation" {
+  enabled = false
+}
+
 #https://github.com/terraform-linters/tflint-ruleset-google
 plugin "google" {
     enabled = true
---
+++ first/local.tf
@@ -0,0 +1,30 @@
+/* 
+ * Manage IAM Members with Terraform Code
+ */
+locals {
+  user_info = {
+    "${module.value.user.root}" = ["owner"]
+  }
+}
+
+locals {
+  keys = distinct(flatten([
+    for user, roles in local.user_info: [
+      for role in roles
+      : "${user}_${role}"
+    ]
+  ]))
+  bindings_by_user = distinct(flatten([
+    for user, roles in local.user_info: [
+      for role in roles: {
+        member = user
+        role   = role
+      }
+    ]
+  ]))
+  bindings = zipmap(
+    local.keys,
+    local.bindings_by_user
+  )
+}
+
